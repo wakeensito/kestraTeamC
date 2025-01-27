Jvald27/kestra

feat(core, jdbc): change the state of a subflow restart parent execution

core/src/main/java/io/kestra/core/queues/QueueService.java
@@ -29,6 +29,8 @@ public class QueueService {
             return null;
         } else if (object.getClass() == ExecutionRunning.class) {
             return ((ExecutionRunning) object).getExecution().getId();
+        } else if (object.getClass() == SubflowExecutionEnd.class) {
+            return ((SubflowExecutionEnd) object).getExecutionId();
         } else {
             throw new IllegalArgumentException("Unknown type '" + object.getClass().getName() + "'");
         }

wakeensito/kestraTeamC

core/src/main/java/io/kestra/core/queues/QueueService.java
@@ -29,6 +29,8 @@ public class QueueService {
             return null;
         } else if (object.getClass() == ExecutionRunning.class) {
             return ((ExecutionRunning) object).getExecution().getId();
+        } else if (object.getClass() == SubflowExecutionEnd.class) {
+            return ((SubflowExecutionEnd) object).getParentExecutionId();
         } else {
             throw new IllegalArgumentException("Unknown type '" + object.getClass().getName() + "'");
         }