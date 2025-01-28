48427f1855270e5013e023d5f13baea75339e7d4
Bart Ledoux <bledoux@kestra.io>
2025-01-28 21:45:34 +0100

    fix: set docId for namespace demo

--- ui/src/components/demo/Namespace.vue
+++ ui/src/components/demo/Namespace.vue
@@ -17,9 +17,13 @@
 <script setup lang="ts">
     import {useI18n} from "vue-i18n";
     import Layout from "./Layout.vue";
+    import {useStore} from "vuex";
     import sourceImg from "../../assets/demo/namespace.png";
 
     const {t} = useI18n();
+    const store = useStore();
+
+    store.commit("doc/setDocId", "namespace.management")
 
     const props = defineProps<{
         tab: string;
