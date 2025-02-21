9ec4d9282b55bc1ae18901f2fd8721b5edac35e8
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 14:48:03 +0100

    fix(core): rename dashboards subgroups

--- core/src/main/java/io/kestra/plugin/core/dashboard/data/package-info.java
+++ core/src/main/java/io/kestra/plugin/core/dashboard/data/package-info.java
@@ -1,4 +1,4 @@
-@PluginSubGroup(title = "dataset", categories = PluginSubGroup.PluginCategory.CORE)
+@PluginSubGroup(title = "Data Filter", categories = PluginSubGroup.PluginCategory.CORE)
 package io.kestra.plugin.core.dashboard.data;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
