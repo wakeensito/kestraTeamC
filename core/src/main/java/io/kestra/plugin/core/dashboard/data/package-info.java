16284e5b95b324ba6532fd1a69a042283c7649da
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 14:35:09 +0100

    fix(core): move package-info.java to proper dashboard packages

---
+++ core/src/main/java/io/kestra/plugin/core/dashboard/data/package-info.java
@@ -0,0 +1,4 @@
+@PluginSubGroup(title = "dataset", categories = PluginSubGroup.PluginCategory.CORE)
+package io.kestra.plugin.core.dashboard.data;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
